8b80267025bba18a5db8ea84894f09e780708e25
Tommy Grealy <tommygrealy@Tommys-MacBook-Pro.local>
2018-12-05 17:34:22 +0000

    added shrink image function

--- face_deteaction_pass_img.py
+++ face_deteaction_pass_img.py
@@ -2,12 +2,35 @@ import numpy as np
 import cv2
 import sys
 
+
+def shrinkImage(imagetoreduce):
+	height, width = imagetoreduce.shape[:2]
+	max_height = 800
+	max_width = 800
+
+	# only shrink if img is bigger than required
+	if max_height < height or max_width < width:
+	    # get scaling factor
+	    print ("Going to")
+	    scaling_factor = max_height / float(height)
+	    if max_width/float(width) < scaling_factor:
+	        scaling_factor = max_width / float(width)
+	    # resize image
+	    cv2.resize(imagetoreduce, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+	    return imagetoreduce
+
+
+
 # We point OpenCV's CascadeClassifier function to where our 
 # classifier (XML file format) is stored
 face_classifier = cv2.CascadeClassifier('Haarcascades/haarcascade_frontalface_default.xml')
 
 # Load our image then convert it to grayscale
 image = cv2.imread(sys.argv[1])
+image2 = shrinkImage(image)
+print (str(type(image)))
+print (str(type(image2)))
+#image = shrinkImage(image)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Our classifier returns the ROI of the detected face as a tuple
@@ -24,5 +47,6 @@ for (x,y,w,h) in faces:
     cv2.rectangle(image, (x,y), (x+w,y+h), (127,0,255), 2)
     cv2.imshow('Face Detection', image)
     cv2.waitKey(0)
-    
+
 cv2.destroyAllWindows()
+
